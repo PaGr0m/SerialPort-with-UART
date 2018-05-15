d8f7d9dda7464268664310a68f0e9bc154557203
Pavel Gromov <pagrom27@gmail.com>
2018-05-15 15:24:02 +1000

    First try to connect

---
+++ Network_Network/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Network_Network
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxPortsName = new System.Windows.Forms.ComboBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(381, 144);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(381, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.myFi);
+            // 
+            // comboBoxPortsName
+            // 
+            this.comboBoxPortsName.FormattingEnabled = true;
+            this.comboBoxPortsName.Location = new System.Drawing.Point(112, 65);
+            this.comboBoxPortsName.Name = "comboBoxPortsName";
+            this.comboBoxPortsName.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxPortsName.TabIndex = 3;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(139, 110);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 4;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.comboBoxPortsName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxPortsName;
+        private System.Windows.Forms.Button buttonSearch;
+    }
+}
+
---
+++ Network_Network/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+
+namespace Network_Network
+{
+    public partial class Form1 : Form
+    {
+        private string PORT_NAME;
+
+        private SerialPort serialPort = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SearchPorts();
+        }
+
+        private void SearchPorts()
+        {
+            foreach (string port in SerialPort.GetPortNames())
+            {
+                comboBoxPortsName.Items.Add(port);
+            }
+        }
+
+        private void ConnectPort()
+        {
+            serialPort.PortName = comboBoxPortsName.Text;
+            listView1.Items.Add(serialPort.PortName);
+        }
+
+        private void DisconnectPort()
+        {
+
+        }
+
+        private void ReadPort()
+        {
+
+        }
+
+        private void WritePort()
+        {
+
+        }
+
+
+        private void myFi(object sender, EventArgs e)
+        {
+            ConnectPort();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            SearchPorts();
+        }
+    }
+}
